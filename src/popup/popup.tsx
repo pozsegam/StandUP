9a50e9bd25723493b8747b542e2fb580effda354
pozsgaimate <pozsgai.mate@evista.hu>
2022-08-14 16:41:16 +0200

    feat: more UI elements

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -6,8 +6,16 @@ import { createRoot } from 'react-dom/client';
 const RowElement = () => {
   return (
     <div className='row-container'>
-      <h1>Time left</h1>
-      <h1>21:24</h1>
+      <h1 className='row-title'>Time left</h1>
+      <h1 className='row-value'>21:24</h1>
+    </div>
+  );
+};
+
+const Button = () => {
+  return (
+    <div className='button-container'>
+      <h1 className='button-content'>Turn off</h1>
     </div>
   );
 };
@@ -16,6 +24,7 @@ const breakTime = (
   <div className='container'>
     <img src='icon.png' alt='' />
     <RowElement />
+    <Button />
   </div>
 );
 
